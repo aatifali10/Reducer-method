ac76409fc747210732be65fef3e7d2763346e3a2
Aatif Ali <aatifwarraich10@gmail.com>
2022-09-13 22:46:05 +0500

    add some more example

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const bestFriends = [
-  { id: "1", name: "Daniyal", age: 24 },
-  { id: "2", name: "Aqib", age: 25 },
-  { id: "3", name: "ALi", age: 23 },
+  { id: "1", name: "Daniyal Manzoor", age: 24 },
+  { id: "2", name: "Aqib Ali", age: 25 },
+  { id: "3", name: "Ali Shair", age: 23 },
 ];
 
 let result;
@@ -19,3 +19,46 @@ console.log(sumAge);
 
 getName = bestFriends.reduce((acc, person) => [...acc, person.name], []);
 console.log(getName);
+
+// convert to id => person lookup (dict)
+
+friendsId = bestFriends.reduce((acc, person) => {
+  return { ...acc, [person.id]: person };
+}, {});
+console.log(friendsId);
+
+// max age
+
+maxAge = bestFriends.reduce((acc, person) => {
+  if (acc === null || person.age > acc) return person.age;
+  return acc;
+}, null);
+console.log(maxAge);
+// min age
+
+minAge = bestFriends.reduce((acc, person) => {
+  if (acc === null || person.age < acc) return person.age;
+  return acc;
+}, null);
+console.log(minAge);
+// find by name
+
+findName = bestFriends.reduce((acc, person) => {
+  if (acc !== null) return acc;
+  if (person.name === "Ali Shair") return person;
+  return null;
+}, null);
+console.log(findName);
+// all over 18
+
+result = bestFriends.reduce((acc, person) => {
+  if (!acc) return false;
+  return person.age > 20;
+}, true);
+console.log(result);
+// any over 18
+
+result = bestFriends.reduce((acc, person) => {
+  if (acc) return true;
+  return person.age > 18;
+}, false);
